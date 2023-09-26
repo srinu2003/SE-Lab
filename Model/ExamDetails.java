b6fcdf23415f7f2f382fcb374c7ebf00239b6b36
Srinivas Rao Tammireddy <tsrin2003@gmail.com>
2023-09-27 00:27:36 +0530

    WEEK 3 27-09-2023 12:27pm

---
+++ Model/ExamDetails.java
@@ -0,0 +1,72 @@
+
+import java.util.*;
+
+/**
+ * @author Srinivas Rao Tammireddy
+ */
+public class ExamDetails {
+
+    /**
+     * Default constructor
+     */
+    public ExamDetails() {
+    }
+
+    /**
+     * 
+     */
+    public void examCode;
+
+    /**
+     * 
+     */
+    public void examID;
+
+    /**
+     * 
+     */
+    public void subjectName;
+
+    /**
+     * 
+     */
+    public void Duration;
+
+    /**
+     * 
+     */
+    public void DateofExam;
+
+    /**
+     * 
+     */
+    public void Fee;
+
+    /**
+     * 
+     */
+    public void ageLimit;
+
+    /**
+     * 
+     */
+    public void Criteria;
+
+    /**
+     * 
+     */
+    public void examCenterName;
+
+    /**
+     * 
+     */
+    public void examCenterCode;
+
+    /**
+     * 
+     */
+    public void addExam() {
+        // TODO implement here
+    }
+
+}
---
+++ Model/Register.java
@@ -0,0 +1,76 @@
+
+import java.util.*;
+
+/**
+ * @author Srinivas Rao Tammireddy
+ */
+public class Register {
+
+    /**
+     * Default constructor
+     */
+    public Register() {
+    }
+
+    /**
+     * 
+     */
+    public void id;
+
+    /**
+     * 
+     */
+    public void examCode;
+
+    /**
+     * 
+     */
+    public void subjectID;
+
+    /**
+     * 
+     */
+    public void NoOfStudents;
+
+    /**
+     * 
+     */
+    public void Fee;
+
+    /**
+     * 
+     */
+    public void DDNo;
+
+    /**
+     * 
+     */
+    public void RegID;
+
+    /**
+     * 
+     */
+    public void examCenterCode;
+
+    /**
+     * 
+     */
+    public void getRegister() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void cancleReg() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void verifyInfo() {
+        // TODO implement here
+    }
+
+}
